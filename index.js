c73cbabdbf87fb14cafaceacf3addffdd8f9a3e8
dev-devendra21 <devendrakumaraa@gmail.com>
2023-11-25 16:25:22 +0000

    create a super league page

---
+++ index.js
@@ -0,0 +1,9 @@
+const element = (
+  //   Write your code here.
+  <div className='bg-container'>
+    <h1 className='heading-text'>Super Over League</h1>
+    <img src='https://assets.ccbp.in/frontend/react-js/rcb-img.png' />
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
